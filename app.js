16f99470ed3226d60707e0ac2434fecd75904ef3
Natalia Cristina <nataliacristina586@gmail.com>
2024-10-03 20:51:28 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-alert("Boas vindas ao jogo do número secreto");
-let numeroMaximo = 50;
-let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
-console.log(numeroSecreto);
-let chute;
-let tentativas = 1;
-// enquanto 
-while (chute != numeroSecreto) {
+// let numeroSecreto = parseInt(Math.random() * 11)
+// let tentativas = 1
+// let chute
 
-    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
-    // se o chute for igual ao numero secreto
-    if (chute == numeroSecreto) {
-        break;
-    } else {
-        if (chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}`);
-        } else {
-            alert(`O número secreto é maior que ${chute}`);
-        }
-        tentativas++;
-    }
-}
-let palavraTentantiva = tentativas > 1 ? "tentativas" : "tentativa";
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentantiva}`);
+// alert('Boas vindas ao jogo do número secreto')
+
+// while (chute != numeroSecreto) {
+//     let chute = prompt('Escolha um número entre 1 e 10')
+//     if (chute == numeroSecreto) {
+//         break
+//     } else {
+//         if (chute > numeroSecreto) {
+//             alert('O número secreto é menor')
+//         } else {
+//             alert('O número secreto é maior')
+//         }
+//     }
+//     tentativas++
+// }
+
+// let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa' 
+// alert(`O número secreto era ${numeroSecreto} e você acertou com apenas ${tentativas} ${palavraTentativa}`)
